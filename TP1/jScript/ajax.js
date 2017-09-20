5f82a42eb2b12e072e31b9cee2cd2d3cba7de936
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2017-09-20 16:29:17 +0100

    Basically done, probably need to review the comments and do the one-page review thing

--- TP1/jScript/ajax.js
+++ TP1/jScript/ajax.js
@@ -1,29 +1,10 @@
-function loadDoc() {
-  /**var xhttp = new XMLHttpRequest();
+function loadDoc(filename) {
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("generalBtn").innerHTML = this.responseText;
+     document.getElementById("general").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "fs_general.php", true);
+  xhttp.open("GET", filename , true);
   xhttp.send();
 }
-
-
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#div1").load("fs_general.php");
-    });
-});
-**/
-
-$.ajax(){
-   		url:"fs_general.php",
-   		type:'GET',
-   		success: function(data){
-      			 //$('[name="factsage"]').html($(data).find('[name="factsage"]').html());
-			alert (data);
-			$(".frame").html(data);
-   		}
-}
-</script>
